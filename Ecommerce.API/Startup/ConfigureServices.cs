4bfa9552fe68c832d3e0abc25218ff4c08ea013f
Mohammedhasmi Patel <patelmohammedhasmi@gmail.com>
2026-05-28 18:48:58 +0530

    initial project

---
+++ Ecommerce.API/Startup/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.API.Startup
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection ConfigureProjectService(this IServiceCollection service)
+        {
+            service.AddControllers();
+            service.AddEndpointsApiExplorer();
+            service.AddSwaggerGen();
+            return service;
+        }
+    }
+}
