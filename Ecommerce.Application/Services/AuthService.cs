4bd77876bb0bf953f74eb3820d8d494018e1209f
Mohammedhasmi Patel <patelmohammedhasmi@gmail.com>
2026-05-28 21:05:11 +0530

    create authservice interface & service

---
+++ Ecommerce.Application/Services/AuthService.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Application.DTO.Auth;
+using Ecommerce.Application.DTO.Common;
+using Ecommerce.Application.ServicesInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Services
+{
+    public class AuthService : IAuthService
+    {
+        public async Task<ApiResponse<string>> RegisterUserService(RegisterUserRequest registerUserRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
